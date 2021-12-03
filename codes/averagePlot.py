db998d58d50049031044b54bdf609c2b6a6fa1a8
Parul664 <parulpsps@gmail.com>
2021-12-03 17:18:38 +0530

    Updated averagePlots to dump average files in common folder

--- codes/averagePlot.py
+++ codes/averagePlot.py
@@ -1,15 +1,18 @@
 from datetime import time
 import os
+from pickle import dump
 import pandas as pd
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 BTP_HOME = "/home/poarul/BTP"
-AVERAGE_FILE = "average.txt"
+AVERAGE_FILE = "average.csv"
+MAIN_AVERAGE_DUMP_FOLDER = "/home/poarul/BTP/averageDump/"
 PERF_RESULTS = os.path.join(BTP_HOME, "perfResults")
 DRAWPLOT_RESULTS = os.path.join(BTP_HOME, "plots")
 
+# Create a new dump folder for dumping all average files by hand
 def temp(savPlot, specific = None):
     benchmarksPresent = os.listdir(path = PERF_RESULTS)
     
@@ -40,7 +43,9 @@ def temp(savPlot, specific = None):
             testRunFiles = os.listdir(path = testCasePath)
             os.chdir(testCasePath)
 
-            averageAndPlot(savPlot, testRunFiles, testCaseDrawPath)
+            # for dumping all files in a single folder
+            dumpName = benchmark + "_" + test + ".csv"
+            averageAndPlot(savPlot, testRunFiles, testCaseDrawPath, dumpName)
         
         if len(testCasesDir) == 0:
 
@@ -50,46 +55,56 @@ def temp(savPlot, specific = None):
             
             os.chdir(benchmarkPath)
             testRunFiles = os.listdir(path = benchmarkPath)
-            averageAndPlot(savPlot, testRunFiles, drawplotPath)
+
+            # for dumping all files in a single folder
+            dumpName = benchmark + ".csv"
+            averageAndPlot(savPlot, testRunFiles, drawplotPath, dumpName)
 
             
 
             
 
-def averageAndPlot(savPlot, testRunFiles, testCaseDrawPath):
-    avgTimeStamp, avgValue = np.array([]), np.array([])
+def averageAndPlot(savPlot, testRunFiles, testCaseDrawPath, fileNameForDump):
+    avgTimeStamp, avgIpc, avgInstructions = np.array([]), np.array([]), np.array([])
     flag = 0
 
     for testRun in testRunFiles:
         print("     Going through ", testRun)                
-        timestamps, values = getDataFromFile(testRun)
+        timestamps, ipcValues, instructionValues = getDataFromFile(testRun)
 
         if not flag:
             avgTimeStamp = timestamps
-            avgValue = values
+            avgIpc = ipcValues
+            avgInstructions = instructionValues
             flag = 1
             continue
 
         a, b = len(avgTimeStamp), len(timestamps)
         if a > b:
             # avgTimeStamp = a
-            values = np.pad(values, (0,a-b), mode = 'constant')
+            ipcValues = np.pad(ipcValues, (0,a-b), mode = 'constant')
+            instructionValues = np.pad(instructionValues, (0,a-b), mode = 'constant')
         elif a < b:
             avgTimeStamp = timestamps
-            avgValue = np.pad(avgValue, (0,b-a), mode="constant")
+            avgIpc = np.pad(avgIpc, (0,b-a), mode="constant")
+            avgInstructions = np.pad(avgInstructions, (0,b-a), mode="constant")
 
         # avgTimeStamp = (avgTimeStamp + timestamps)/2
-        avgValue = (avgValue + values)/2
+        avgIpc = (avgIpc + ipcValues)/2
+        avgInstructions = (avgInstructions + instructionValues)/2
 
-    combinedData = np.vstack((avgTimeStamp, avgValue)).T
-    np.savetxt(AVERAGE_FILE, combinedData, delimiter=',')
+    combinedTimeIpc = np.vstack((avgTimeStamp, avgIpc))
+    combinedTimeIpcInstructions = np.vstack((combinedTimeIpc, avgInstructions)).T
+    np.savetxt(AVERAGE_FILE, combinedTimeIpcInstructions, delimiter=',')
+    np.savetxt(os.path.join(MAIN_AVERAGE_DUMP_FOLDER,fileNameForDump), combinedTimeIpcInstructions, delimiter=',')
+
     # print("Path to save for testCase ",test, " is :", testCaseDrawPath)
     if(savPlot):
         print(testCaseDrawPath)
         if not os.path.exists(testCaseDrawPath):
             os.makedirs(testCaseDrawPath)
         final_plot_save_path = os.path.join(testCaseDrawPath,"averagePlot.jpg")
-        plotLineGraph(avgTimeStamp, avgValue, final_plot_save_path)
+        plotLineGraph(avgTimeStamp, avgIpc, final_plot_save_path)
 
 
 
@@ -122,12 +137,14 @@ def getDataFromFile(fileName):
     df = df[df['div_value'].notna()]
 
     timestamps = df['timestamp'].values
-    values = df['div_value']
+    ipc = df['div_value']
+    instructions = df['value']
     # plt.plot(timestamps, values)
     # plt.show()
 
-    return timestamps, values
+    return timestamps, ipc, instructions
 
+# dumping average into a folder always enabled
 drawPlot = sys.argv[1]
 if len(sys.argv) > 2:
     temp(drawPlot, sys.argv[2])
